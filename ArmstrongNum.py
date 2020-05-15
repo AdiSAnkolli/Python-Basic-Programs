398aa09375e0e5b6f8a8b2a7dd37be5ca4e574ed
AdiSAnkolli <59432457+AdiSAnkolli@users.noreply.github.com>
2020-05-15 15:59:00 +0530

    Update ArmstrongNum.py

--- ArmstrongNum.py
+++ ArmstrongNum.py
@@ -5,7 +5,7 @@ for i in range(1001):
 #As numerical variables do not contain length property, they need to be converted to string
     while(i!=0):
         digit=i%10
-        result=result+digit**nee
+        result=result+digit**n
         i=i//10
 # // stands for division truncate, which returns integer part of the quotient after division, that is the number before decimal point
         if num==result:
